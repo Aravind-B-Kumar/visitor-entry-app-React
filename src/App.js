fa345b68dd6261589cfe070bd39d163788c98a2c
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-08-13 11:27:02 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import AddVisitor from './components/AddVisitor';
 import ViewAllVisitors from './components/ViewAllVisitors';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      <ViewAllVisitors/>
-    </div>
+    
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<AddVisitor/>}/>
+      <Route path='/getvisitor' element={<ViewAllVisitors/>}/>
+      <Route path='/addvisitor' element={<AddVisitor/>}/>
+     </Routes>
+     </BrowserRouter>
+   
   );
 }
 
